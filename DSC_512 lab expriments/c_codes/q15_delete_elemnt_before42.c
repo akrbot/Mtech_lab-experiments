dc4b2c45fa27e41277730eac9aa4410e75fad894
akrbot <akr.workspace@gmail.com>
2024-02-11 18:00:20 +0530

    added q 14 and 15

--- DSC_512 lab expriments/c_codes/q15_delete_elemnt_before42.c
+++ DSC_512 lab expriments/c_codes/q15_delete_elemnt_before42.c
@@ -9,21 +9,18 @@ struct node{
 
 int create_doubly_ll();
 void traverse_fw();
-int insert_after_24(int size, int element);
+int delete_after_42(int size);
 
 void main(){
     int count = create_doubly_ll();
-    int element;
-    scanf("%d",&element);
-    int flag = insert_after_24(count,element);
+    int flag = delete_after_42(count);
     if(flag) traverse_fw();
 }
 
-
 void traverse_fw(){
     struct node *temp = head;
-    if (head == NULL) printf("underflow");
-    while (temp != NULL){
+    if(head == NULL) printf("underflow");
+    while(temp != NULL){
         printf("%d ",temp->data);
         temp = temp->next;
     }
@@ -47,61 +44,70 @@ int create_doubly_ll(){
     head->data = data;
     head->prev = NULL;
     head->next = NULL;
-    count ++;
     
     prev_node = head;
     while(1){
         scanf("%d",&data);
         if(data == -999) break;
         new_node = (struct node*)malloc(sizeof(struct node));
-        if(new_node == NULL){
+        if (new_node == NULL){
             printf("memory allocation error");
             return 0;
         }
-        new_node->data = data;
+        new_node->data =data;
         new_node->prev = prev_node;
         new_node->next = NULL;
         
         prev_node->next = new_node;
         prev_node = new_node;
-        count++;
+        count ++;
     }
     end = prev_node;
     return count;
 }
 
-int insert_after_24(int size,int element){
-    //find nodes with data 24 and store it in an array;
+int delete_after_42(int size){
+    //find nodes with data 42 and store it in an array;
     struct node *node_arr[size];
-    struct node *temp = head, *new_node;
+    struct node *temp = head;
     int i = 0;
     while(temp != NULL){
-        if(temp->data == 24){
+        if(temp->data == 42){
             node_arr[i] = temp;
             i++;
         }
         temp = temp->next;
     }
-    //if 24 is not found i == 0 so do nothin just return
+    //if 42 is not found i == 0 so do nothin just return
     if(i == 0){
-        printf("At least one element must be 24");
+        printf("At least one element must be 42");
         return 0;
     }
-    //if i>=2 means more than 1 occurence of 24
-    //so ask for user input to insert after the given index
-    else if(i>=2){
-        printf("%d occurrence of 24 found. Where should the insertion occur?",i);
+    //if i>=2 means more than 1 occurence of 42
+    //so ask for user input to delete before given index;
+    else if (i>=2){
+        printf("%d occurrence of 42 found. Where should the deletion occur?\n",i);
         int input;
         scanf("%d",&input);
         i = input;
     }
     i = i-1;
-    //create new node and insert after 24
-    new_node = (struct node*)malloc(sizeof(struct node));
-    temp = node_arr[i]->next;
-    new_node->data = element;
-    new_node->next = temp;
-    new_node->prev = node_arr[i];
-    node_arr[i]->next = new_node;
+    //delete node before 42
+    temp = node_arr[i]->prev;
+    if(temp == NULL){
+        printf("No element before 42");
+        return 0;
+    }
+    //if node to be deleted is head then make head point to next node
+    //delete node and free memory
+    if (temp == head){
+        head = temp->next;
+        head->prev = NULL;
+        free(temp);
+        return 1;
+    }
+    temp->next->prev = temp->prev;
+    temp->prev->next = temp->next;
+    free(temp);
     return 1;
 }
